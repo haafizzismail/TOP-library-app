303ea86873a262afb23b8b3b01fc971bd0f89af9
Haafizz Ismail <mdnurhaafizz@gmail.com>
2022-04-24 22:59:16 +0800

    log all books in library array

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let myLibrary = [];
+let myLibrary = ["Atomic Habits", "Dogman", "The Stand"];
 
 function Book() {
     this.title = title;
 }
 
-function addBookToLibrary() {
-    let newBook = prompt("Title of book?");
-    myLibrary.push(newBook);
+// function addBookToLibrary() {
+//     let newBook = prompt("Title of book?");
+//     myLibrary.push(newBook);
+// }
+
+function displayBooks() {
+    myLibrary.forEach(book => console.log(book));
 }
